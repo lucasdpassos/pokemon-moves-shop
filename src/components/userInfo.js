5771d3fb7cae940078c3ab7df9b97f8eadcc6435
Lucas Dezerto Passos <lucas.depassos@yahoo.com.br>
2023-12-14 11:49:11 -0300

    adjusted content position and added icons

--- src/components/userInfo.js
+++ src/components/userInfo.js
@@ -4,6 +4,7 @@ import { fetchPokemon, selectPokemon } from '../features/userSlice';
 import PowerCard from '../components/powerCard';
 import PowerCardSkeleton from '../components/powerCardSkeleton';
 import CartComponent from './CartComponent';
+import SearchIcon from '@mui/icons-material/Search';
 
 const UserInfo = () => {
   const dispatch = useDispatch();
@@ -37,30 +38,36 @@ const UserInfo = () => {
   return (
     <div >
       <div className='flex flex-col'>
-      <h2>Buscar Pokémon por Nome</h2>
+      <h2 className='mx-auto mb-5 font-bold'>Digite o nome do Pokémon</h2>
+      <div className='flex flex-row align-middle justify-center'>
       <label>
         Pokémon:
         <input
           type="text"
           value={inputPokemon}
           onChange={(e) => setInputPokemon(e.target.value)}
+          className='border-black border ml-1 mr-1'
         />
       </label>
-      <button className='bg-slate-500 text-white w-16 h-7 shadow-lg rounded-md hover:bg-slate-900' onClick={handleSearch}>Buscar</button>
+      <button className='bg-slate-500 text-white w-16 h-7 shadow-lg rounded-md hover:bg-slate-900' onClick={handleSearch}><SearchIcon /></button>
+      </div>
       <CartComponent />
       
       
       </div>
       <div>
+        
         <h3>Informações do Pokémon:</h3>
         {pokemon.pokemonLoading && <PowerCardSkeleton />}
         {pokemon.name && !pokemon.pokemonLoading ? (
           <div>
-            <img className='animate-bounce' src={pokemon.avatar} alt='poke' />
+            <div className='flex flex-col align-middle mx-auto justify-center items-center'>
+            <img className='animate-bounce max-w-[60px] max-h-14' src={pokemon.avatar} alt='poke' />
             <p>Name: {pokemon.name}</p>
             <p>ID: {pokemon.id}</p>
             <p>Location Area Encounters: {pokemon.locationAreaEncounters}</p>
             <h3>Moves:</h3>
+            </div>
             {renderMoveGroups()}
           </div>
         ) : (
